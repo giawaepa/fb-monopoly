3292ae82f2ca93e419907cf54dfd89d27f3570a8
hemetsu <hemetsu@gmail.com>
2011-10-26 20:29:13 +0000

    Latest Code Update: - Multiplayer functionality - Chat functionality - Fixed arrow/land positions

--- scripts/channel.js
+++ scripts/channel.js
@@ -3,9 +3,6 @@
  * Description: Handles GAE channel connections: getToken, openChannel, onOpen, onMessage, sendMessage
  */
 
-//Global variables
-
-
 //Initialization - Get token and open channel
 function initialize() {
 	var token = getToken();
@@ -46,8 +43,6 @@ function onOpened() {
 	console.log("[STATUS] Channel established.");
 	$("#loading_container").html("");
 	$("#loading_container").html("Game Channel connected...");
-	//Hide Loading container
-	$("#loading_container").css("display","none");
 	
 	//Request for current location of user
 	var xhr = new XMLHttpRequest(); 
@@ -76,32 +71,131 @@ function onMessage(m) {
 		prevtile[1] = chartile[1];
     	
     	//Map character tile to location on page
-    	var init_top = 400;
-    	var init_left = 125; 
     	var left = 0,top = 0;
-    	for (i=0;i<chartile[0]-1;i++) {
-    		left+=30;
-    		top+=15;
-    	}
-    	for (j=0;j<chartile[1]-1;j++) {
-    		left+=30;
-    		top-=15;
-    	}
+
+      	left = 30*(chartile[0] + chartile[1]-2);
+    	top = (15*chartile[0])-(15*chartile[1]) + 100;
     	
     	//Position character
     	$('#character').css('left',init_left + left).css('top',init_top + top);
+    	
+    	//Hide Loading container
+    	$("#loading_container").css("display","none");
+    	
     } else if (newMessage.method == "updateUsers") {
     	console.log("[STATUS] Updating User List...");
-    	list = "";
-    	//location = newMessage.userlocations;
+    	var list = "";
+    	
+    	//Create container for all other characters
+    	//$('<div id="character_other" style="position:relative;top:0px;left0px;"></div>').prependTo("#map");
+    	
     	for (i=0;i<newMessage.userlist.length;i++) {
     		list += newMessage.userlist[i].name + "<br/>";
+    		
+    		//If user ID is not current user then add a new character in
+    		if ((newMessage.userlist[i].id != user_id)&&($('#char_'+newMessage.userlist[i].id).length == 0)) {
+	    		//Parse location
+	    	   	end = newMessage.userlist[i].location.indexOf('.');
+	        	var userx = parseInt(newMessage.userlist[i].location.substring(0,end));
+	        	var usery = parseInt(newMessage.userlist[i].location.substring(end+1));
+	    		
+	    		//Create character element
+	    		var left = 30*(userx + usery-2) + init_left;
+	    		var top = (15*userx)-(15*usery) + init_top;
+	    		var el = '<div id="char_'+newMessage.userlist[i].id+'" class="char_image" style="top:'+top+'px; left:'+left+'px;"></div>';
+	    		//Add character
+	    		$(el).appendTo('#character_other');
+    		}
     	}
     	console.log("[INFO] List: " + list);
     	$("#userlist").html(list);
+    } else if (newMessage.method == "updateOneUser") {
+    	console.log("[STATUS] Update Other User location");
+    	
+		//Parse location
+	   	end = newMessage.location.indexOf('.');
+    	var userx = parseInt(newMessage.location.substring(0,end));
+    	var usery = parseInt(newMessage.location.substring(end+1));
+		var left = 30*(userx + usery-2) + init_left;
+		var top = (15*userx)-(15*usery) + init_top;
+    	
+    	//Update User location
+    	$('#char_'+newMessage.userid).css("top",top+"px").css("left",left+"px");
+    } else if (newMessage.method == "deleteUser") {
+    	console.log("[STATUS] Deleting User");
+    	//Remove element from DOM
+    	$('#char_'+newMessage.userid).remove();
+    	
+    	//Clear User list
+    	$("#userlist").html('');
+    	
+        //Request for list of online users
+    	var xhr = new XMLHttpRequest(); 
+        xhr.open('GET', '/client?method=updateUsers', true);
+        xhr.send(null); 
+    	
+    } else if (newMessage.method == "chatMessage") {   	
+    	if (newMessage.userid != user_id) {
+        	console.log("[STATUS] Update User Message");
+	    	//Add new chat bubble
+        	console.log(newMessage.userid + " said: " + newMessage.message);
+        	
+        	//Add bubble if it's not there. Otherwise change the message.
+        	if ($('#bubble_'+newMessage.userid).length == 0) {
+        		$('#char_'+newMessage.userid).prepend('<div id="bubble_'+newMessage.userid+'" class="bubble">'+newMessage.message+'</div>');
+        	} else {
+        		$('#bubble_'+newMessage.userid).html('');
+        		$('#bubble_'+newMessage.userid).html(newMessage.message);
+        	}
+	    	
+			$('#bubble_'+newMessage.userid).animate({
+			    opacity: 1,
+			    top: '-=10'
+			  }, 100, function() {
+				  setTimeout(function() {
+					  $('#bubble_'+newMessage.userid).animate({
+						    opacity: 0
+					  }, 100);
+					  $('#bubble_'+newMessage.userid).remove();
+				  },5000);
+			  });
+    	}
     } else {
     	console.log("[STATUS] Method not handled.");
     }
+};
+
+function sendMessage() {
+	//Get the message
+	message = $('#message_box').val();
+	
+	//Add bubble if it's not there. Otherwise change the message.
+	if ($('#bubble_'+user_id).length == 0) {
+		$('#character').prepend('<div id="bubble_'+user_id+'" class="bubble">'+message+'</div>');
+
+		$('#bubble_'+user_id).animate({
+		    opacity: 1,
+		    top: '-=10'
+		  }, 100, function() {
+			  setTimeout(function() {
+				  $('#bubble_'+user_id).animate({
+					    opacity: 0
+				  }, 100);
+				  $('#bubble_'+user_id).remove();
+			  },5000);
+		  });
+	} else {
+		$('#bubble_'+user_id).html('');
+		$('#bubble_'+user_id).html(newMessage.message);
+	}
+
+	//Clear the message box
+	$('#message_box').val('');
+	
+    //Send message to clients
+	var xhr = new XMLHttpRequest(); 
+    xhr.open('GET', '/client?method=sendMsg&userid='+user_id+'&chatMessage='+message, true);
+    xhr.send(null);
 };
 
 function saveLocation(location) {
